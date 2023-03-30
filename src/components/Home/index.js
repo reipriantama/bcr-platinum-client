3c29558403a8ddc8a1215c10f9292e034e19e9e9
HelloCaptainAufa737 <42775351+AufaAnasin@users.noreply.github.com>
2023-03-30 23:01:45 +0700

    Add Third Section or Why us Section

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -2,7 +2,15 @@ import React from 'react'
 import './index.scss'
 import MerC from '../img/img_car.png'
 import serviceBla from '../img/img_service.png'
-import CardWhyUs from '../CardWhyUsComponent'
+import WhyUsCard from '../WhyUsCardComponent'
+
+// import icons
+
+import ThumbsIcon from '../img/Icons/icon_thumbs.png'
+import TwentyForHour from '../img/Icons/icon_24hrs.png'
+import PriceIcon from '../img/Icons/icon_price.png'
+import ProfessionalIcon from '../img/Icons/icon_professional.png'
+
 
 function Home() {
   return (
@@ -60,25 +68,40 @@ function Home() {
           <div className="tag-why-us">Why Us?</div>
           <div className="why-us-desc">Mengapa harus pilih Binar Car Rental?</div>
           <div className="card-container">
-            <div className="container text-center">
+            <div className="container text-center" style={{padding: "0", margin: "0"}}>
               <div className="row">
-                    <div className="col">
-                    <CardWhyUs />
-                    </div>
-                    <div className="col">
-                    <CardWhyUs />
-                    </div>
-                    <div className="col">
-                    <CardWhyUs />
-                    </div>
-                    <div className="col">
-                    <CardWhyUs />
-                    </div>
+                <div className="col">
+                    <WhyUsCard
+                    cardIcon={ThumbsIcon}
+                    cardTitle='Mobil Lengkap'
+                    cardText='Tersedia banyak pilihan mobil, kondisi masih baru, bersih dan terawat'
+                    />
+                </div>
+                <div className="col">
+                <WhyUsCard
+                    cardIcon={PriceIcon}
+                    cardTitle='Harga Murah'
+                    cardText='Harga murah dan bersaing, bisa bandingkan harga kami dengan rental mobil lain'
+                    />
+                </div>
+                <div className="col">
+                <WhyUsCard
+                    cardIcon={TwentyForHour}
+                    cardTitle='Layanan 24 Jam'
+                    cardText='Siap melayani kebutuhan Anda selama 24 jam nonstop. Kami juga tersedia di akhir minggu'
+                    />
+                </div>
+                <div className="col">
+                <WhyUsCard
+                    cardIcon={ProfessionalIcon}
+                    cardTitle='Sopir Profesional'
+                    cardText='Sopir yang profesional, berpengalaman, jujur, ramah dan selalu tepat waktu'
+                    />
+                </div>
               </div>
             </div>
           </div>
         </div>
-        
       </div>
     </section>
 
---
+++ src/components/WhyUsCardComponent/index.js
@@ -0,0 +1,19 @@
+import React from 'react'
+// import ThumbsUp from '../img/Icons/icon_thumbs.png'
+import './index.scss'
+
+function WhyUsCard({cardIcon, cardTitle, cardText}) {
+  return (
+    <>
+    <div class="card">
+        <div class="card-body">
+            <img src={cardIcon} alt=". ." className='card-icon' />
+            <div class="card-title">{cardTitle}</div>
+            <div  class="card-text">{cardText}</div>
+        </div>
+    </div>
+    </>
+  )
+}
+
+export default WhyUsCard
