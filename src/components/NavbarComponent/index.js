f279ce389e348ce801b23a292caf3f73aa01d4f1
HelloCaptainAufa737 <42775351+AufaAnasin@users.noreply.github.com>
2023-06-17 01:59:33 +0700

    navbar fixed

--- src/components/NavbarComponent/index.js
+++ src/components/NavbarComponent/index.js
@@ -1,43 +1,28 @@
 import React from 'react'
-import { Navbar, Offcanvas, Container, Nav } from 'react-bootstrap'
-import Logo from '../img/logo.png'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import './index.scss'
 
 const NavbarComponent = () => {
     return (
         <>
-      {['xl'].map((expand) => (
-        <Navbar key={expand} bg="light" expand={expand} className="mb-3">
-          <Container fluid>
-            <Navbar.Brand href="#"><img
-            src={Logo}              
-            width="60"
-            height="30"
-            className="d-inline-block align-top"
-            alt="React Bootstrap logo"/>
-            </Navbar.Brand>
-            <Navbar.Toggle aria-controls={`offcanvasNavbar-expand-${expand}`} />
-            <Navbar.Offcanvas
-              id={`offcanvasNavbar-expand-${expand}`}
-              aria-labelledby={`offcanvasNavbarLabel-expand-${expand}`}
-              placement="end"
-            >
-              <Offcanvas.Header closeButton>
-                <Offcanvas.Title id={`offcanvasNavbarLabel-expand-${expand}`}>
-                  Offcanvas
-                </Offcanvas.Title>
-              </Offcanvas.Header>
-              <Offcanvas.Body>
-                <Nav className="justify-content-end flex-grow-1 pe-3">
-                  <Nav.Link href="#action1">Our Services</Nav.Link>
-                  <Nav.Link href="#action2">Why Us</Nav.Link>
-                  <Nav.Link href="#action3">Testimonial</Nav.Link>
-                  <Nav.Link href="#action3">FAQ</Nav.Link>
+        <Navbar collapseOnSelect expand="lg" className="navbar-main">
+            <Container>
+              <Navbar.Brand href="#home">Logo</Navbar.Brand>
+              <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+              <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="me-auto">
                 </Nav>
-              </Offcanvas.Body>
-            </Navbar.Offcanvas>
-          </Container>
+                <Nav>
+                  <Nav.Link href="#deets">Our Service</Nav.Link>
+                  <Nav.Link href="#memes">Why Us</Nav.Link>
+                  <Nav.Link href="#memes">Testimonial</Nav.Link>
+                  <Nav.Link href="#memes">FAQ</Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
         </Navbar>
-      ))}
         </>
     )
 }
