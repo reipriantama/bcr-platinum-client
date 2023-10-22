225c0bcb4c5215ef8c66ea74669c2afc85ea5f75
vinardl <vina.ardeliaeff@gmail.com>
2023-10-22 20:43:27 +0700

    UI eticket

--- src/components/page/Client/Pembayaran/SelesaikanPembayaran/KonfirmasiPembayaran/index.jsx
+++ src/components/page/Client/Pembayaran/SelesaikanPembayaran/KonfirmasiPembayaran/index.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useLocation } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import style from "./index.module.css";
+import { Link } from 'react-router-dom';
 
 const KonfirmasiPembayaran = () => {
     return(
@@ -10,7 +11,9 @@ const KonfirmasiPembayaran = () => {
                 <h6>Klik konfirmasi pembayaran untuk mempercepat proses pengecekan</h6>
             </div>
             <div className={``}>
+                <Link to="/eticket">
                 <button className="w-100 pt-1 pb-1 " style={{backgroundColor: "#5CB85F", color: "#FFF"}}>Konfirmasi Pembayaran</button>
+                </Link>
             </div>
         </div>
     );
