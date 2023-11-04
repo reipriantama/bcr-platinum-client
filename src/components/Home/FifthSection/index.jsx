22497402ca0fbb0df810a1c8ccc3e002634a1816
charvinze <charvinze@gmail.com>
2023-11-05 00:33:42 +0700

    membuat modal untuk mencegah User melakukan order tanpa Sign In/Register

--- src/components/Home/FifthSection/index.jsx
+++ src/components/Home/FifthSection/index.jsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import './index.css'
+import React from 'react';
+import { useState } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import './index.css';
+import style from "./index1.module.css";
 
 function FifthSection() {
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem("token"));
+  const [displayModal, setDisplayModal] = useState(false);
+
+  const navigate = useNavigate();
+
+  const displayMessage = () => {
+      setDisplayModal(true);
+      setTimeout(() => {
+        setDisplayModal(false);
+      }, 2500);
+  };
+
+  const checkAuthentication = () => {
+      if(isAuthenticated) {
+          navigate("/find");
+      }
+      else {
+        displayMessage();
+      }
+  };
+
+
   return (
     <>
+    {
+      displayModal
+       &&
+      <div className={`${style.modal}`}>
+        <div className={`${style.modal_container}`}>
+            <span className={`${style.popup}`}>Harap Sign In atau Registrasi terlebih dahulu.</span>
+        </div>
+     </div>
+    }
     <div className="container">
         <div className="row fifth">
         <div className="col fourth">
@@ -17,8 +50,8 @@ function FifthSection() {
                 <div className="text-second"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, 
             sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. </div></div>
             </div>
-            <Link to="/find">
-            <button type="button" class="btn btn-success second">
+            <Link>
+            <button type="button" class="btn btn-success second" onClick={checkAuthentication}>
             Mulai Sewa Mobil</button></Link>
         </div>
         </div>
